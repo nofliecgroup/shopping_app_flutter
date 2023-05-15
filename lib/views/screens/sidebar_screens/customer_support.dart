bb34ffd4f3e60d5cf0cc7c885dd2cd6ea1e04bbb
Momodou NJIE <mnjie@nofliegroup.com>
2023-05-15 12:54:32 +0200

    modified datetime format

--- lib/views/screens/sidebar_screens/customer_support.dart
+++ lib/views/screens/sidebar_screens/customer_support.dart
@@ -1,13 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 
-class CustomerSupportCenter extends StatelessWidget {
+class CustomerSupportCenter extends StatefulWidget {
   static const String routeName = '/CustomerSupportCenter';
 
   const CustomerSupportCenter({super.key});
 
   @override
+  State<CustomerSupportCenter> createState() => _CustomerSupportCenterState();
+}
+
+class _CustomerSupportCenterState extends State<CustomerSupportCenter> {
+/*   String formattedTime = DateFormat('kk:mm').format(DateTime.now());
+  String hour = DateFormat('a').format(DateTime.now()); */
+  late Timer _timer;
+
+  @override
   Widget build(BuildContext context) {
     final date = DateTime.now();
+    late String _timeString = "${date.hour} : ${date.minute} :${date.second}";
+
+    void _getCurrentTime() {
+      setState(() {
+        _timeString =
+            "${DateTime.now().hour} : ${DateTime.now().minute} :${DateTime.now().second}";
+      });
+    }
+
+    @override
+    void initState() {
+      _timeString =
+          "${DateTime.now().hour} : ${DateTime.now().minute} :${DateTime.now().second}";
+      Timer.periodic(Duration(seconds: 1), (Timer t) => _getCurrentTime());
+      super.initState();
+    }
 
     return SingleChildScrollView(
       child: Container(
@@ -16,7 +43,7 @@ class CustomerSupportCenter extends StatelessWidget {
         padding: const EdgeInsets.all(10),
         child: Center(
           child: Text(
-            date.toString(),
+            _timeString.toString(),
             style: TextStyle(
               fontSize: 30,
               fontWeight: FontWeight.bold,
