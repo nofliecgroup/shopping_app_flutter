0cf5e64d5d0ad343f641886a3d4e1df843f8a9c4
Momodou NJIE <mnjie@nofliegroup.com>
2023-05-15 11:15:29 +0200

    changed the text to display the date

---
+++ lib/views/screens/sidebar_screens/customer_support.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomerSupportCenter extends StatelessWidget {
+  static const String routeName = '/CustomerSupportCenter';
+
+  const CustomerSupportCenter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final date = DateTime.now();
+
+    return SingleChildScrollView(
+      child: Container(
+        color: Color.fromRGBO(105, 159, 149, 0.996),
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(10),
+        child: Center(
+          child: Text(
+            date.toString(),
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
