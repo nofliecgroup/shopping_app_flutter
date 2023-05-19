e0a2317dd2b1d0f156510cbfc2046fc7978931c8
Momodou Njie <mnjie677@gmail.com>
2023-05-19 02:55:14 +0200

    work in progress on the witdrawal page

--- lib/views/screens/sidebar_screens/vendor_screen.dart
+++ lib/views/screens/sidebar_screens/vendor_screen.dart
@@ -4,19 +4,21 @@ class VendorScreen extends StatelessWidget {
   static const String routeName =  '/VendorScreen';
 
 
-  Widget _rowHeader() {
+  Widget _rowHeader(String header, int flex) {
   return Expanded(
+    flex: 1,
     child: Container(
-    color: Color.fromRGBO(177, 12, 48, 1.0),
-    alignment: Alignment.center,
-    padding: const EdgeInsets.all(10),
-    child: const Text(
-      'Manage Vendors',
-      style: TextStyle(
-        fontSize: 30,
-        fontWeight: FontWeight.bold, color: Colors.white,
+      decoration: BoxDecoration(
+        color: Color.fromARGB(255, 172, 111, 7),
+        border: Border.all(
+          color: Colors.grey.shade700, 
+        ), 
       ),
+    child: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(header, textAlign: TextAlign.center, style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold,  color: Colors.white),),
     ),
+    
   ),
   );
 }
@@ -24,17 +26,34 @@ class VendorScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        color: Color.fromRGBO(177, 12, 48, 1.0),
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Manage Vendors',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold, color: Colors.white,
+      child: Column(
+        children: [
+          Container(
+           color: Color.fromARGB(255, 232, 4, 137),
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Manage Vendors',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold, color: Colors.white,
+              ),
+            ),
           ),
-        ),
+
+Row(
+  children: [
+    _rowHeader('Vendor Profile', 1),
+    _rowHeader('Vendor Name', 1),
+    _rowHeader('Vendor Address', 3),
+    _rowHeader('Vendor Phone', 1),
+    _rowHeader('Vendor Email', 2),
+    _rowHeader('Vendor Status', 1),
+    _rowHeader('Vendor Action', 1),
+  ],
+)
+
+        ],
       ),
     );
   }
--- lib/views/screens/sidebar_screens/withdrawal_screen.dart
+++ lib/views/screens/sidebar_screens/withdrawal_screen.dart
@@ -1,22 +1,62 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class WithdrawaScreen extends StatelessWidget {
   static const String routeName = '/WithdrawalScreen';
 
+  Widget _rowHeader(String header, int flex) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color.fromARGB(255, 4, 156, 70),
+          border: Border.all(
+            color: Color.fromARGB(255, 190, 21, 21),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            header,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 12, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Container(
-        color: Color.fromRGBO(234, 15, 84, 0.988),
-        alignment: Alignment.center,
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Financial transactions are displayed....',
-          style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
+      child: Column(
+        children: [
+          Container(
+            color: Color.fromARGB(255, 232, 4, 137),
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(10),
+            child: const Text(
+              'Withdrawal Requests',
+              style: TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
           ),
-        ),
+          Row(
+            children: [
+              _rowHeader('Vendor Profile', 1),
+              _rowHeader('Vendor Name', 1),
+              _rowHeader('Vendor Address', 3),
+              _rowHeader('Vendor Phone', 1),
+              _rowHeader('Vendor Email', 2),
+              _rowHeader('Vendor Status', 1),
+              _rowHeader('Vendor Action', 1),
+            ],
+          )
+        ],
       ),
     );
   }
